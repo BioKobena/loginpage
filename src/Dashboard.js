fbb0d34b098ff5ec423801c0cc4ef78b62d503c1
BioKobena <biokobena07@gmail.com>
2022-12-30 14:58:25 +0100

    Fin de la page d'inscription

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,57 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
+import { firebase } from "../config";
 
 const Dashboard = () => {
-  return (
-    <Text>Dashboard</Text>
-  )
-}
+  const [name, setName] = useState([]);
 
-export default Dashboard
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists) {
+          setName(snapshot.data());
+        } else {
+          console.log("L'utilisateur n'existe pas");
+        }
+      });
+  }, []);
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+        Bonjour, {name.firstName}
+      </Text>
+      <TouchableOpacity
+        onPress={()=> firebase.auth().signOut()}
+        style={styles.button}
+      >
+        <Text style={{ fontWeight: "bold", fontSize: 22 }}>
+          Se deconnecter
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  button: {
+    marginTop: 50,
+    height: 70,
+    width: 250,
+    backgroundColor: "#026efd",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+});
+
+export default Dashboard;
